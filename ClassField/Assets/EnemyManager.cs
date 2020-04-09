6243f82054e153a2929d04ffcc1cb96e64e0b782
Qiu00414 <63327431+Qiu00414@users.noreply.github.com>
2020-04-09 12:51:35 +0800

    ClassField

---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [Header("怪物一號")]
+    public enemy enemy1;
+
+    [Header("怪物二號")]
+    public enemy enemy2;
+
+    private void Start()
+    { 
+    }
+}
+
